f63bac878283e8fa26278f3c0400fc06e2ee20bc
rockets23 <90385440+rockets23@users.noreply.github.com>
2022-03-21 15:18:18 +0100

    Add files via upload

---
+++ data_cleaning.sql
@@ -0,0 +1,103 @@
+--CLEANING DATA WITH SQL
+
+SELECT* 
+from Covid_Potfolio_project..NashvilleHousing
+
+
+--Standarizing date format 
+
+ALTER TABLE NashvilleHousing
+ADD SaleDateConverted Date
+
+UPDATE NashvilleHousing
+set SaleDateConverted = CONVERT(date, SaleDate)
+
+
+
+--Populate Property Address data ( where it is NULL)
+
+SELECT X.ParcelID, X.PropertyAddress, Y.ParcelID, Y.PropertyAddress, ISNULL(X.PropertyAddress, Y.PropertyAddress)
+FROM Covid_Potfolio_project..NashvilleHousing X 
+JOIN Covid_Potfolio_project..NashvilleHousing Y
+ON X.ParcelID = Y.ParcelID
+AND X.[UniqueID ] <> Y.[UniqueID ]
+WHERE X.PropertyAddress IS NULL
+
+UPDATE X
+SET PropertyAddress = ISNULL(X.PropertyAddress, Y.PropertyAddress)
+FROM Covid_Potfolio_project..NashvilleHousing X 
+JOIN Covid_Potfolio_project..NashvilleHousing Y
+ON X.ParcelID = Y.ParcelID
+AND X.[UniqueID ] <> Y.[UniqueID ]
+WHERE X.PropertyAddress IS NULL
+
+
+Select PropertyAddress
+from Covid_Potfolio_project..NashvilleHousing
+where PropertyAddress is null   --RETURN AN AMPTY TABLE CAUSE ALL THE NULLS HAVE BEEN POPULATED
+
+
+
+
+
+--Breaking out addresses into 3 columns (Address, City, State)
+select
+PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3) AS Address,
+PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2) AS City,
+PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1) AS State
+
+
+from Covid_Potfolio_project..NashvilleHousing
+
+
+ALter table NashvilleHousing
+add AddressN NVARCHAR(255), City NVARCHAR(255), State NVARCHAR(255)
+
+Update NashvilleHousing
+SET AddressN = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3),
+City = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2),
+State =  PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
+
+
+
+
+--Replace N and Y by Yes and NO in SoldVacant
+
+select SoldAsVacant, 
+CASE WHEN SoldAsVacant= 'Y' then 'Yes'
+     WHEN SoldAsVacant= 'N' THEN 'No'
+	 ELSE SoldAsVacant end AS Corrected
+	 from Covid_Potfolio_project..NashvilleHousing
+
+	 UPDATE Covid_Potfolio_project..NashvilleHousing
+	 SET SoldAsVacant = CASE WHEN SoldAsVacant= 'Y' then 'Yes'
+     WHEN SoldAsVacant= 'N' THEN 'No'
+	 ELSE SoldAsVacant end
+
+	 select SoldAsVacant
+	 from Covid_Potfolio_project..NashvilleHousing
+	 where SoldAsVacant = 'Yes' or SoldAsVacant = 'No'
+
+
+	 
+	 --Find Duplicates
+	 
+	 with duplicates AS 
+	( SELECT   SaleDate, OwnerName, PropertyAddress, SalePrice,LegalReference, COUNT(*) as NUM
+FROM Covid_Potfolio_project..NashvilleHousing
+GROUP BY SaleDate, OwnerName, PropertyAddress, SalePrice, LegalReference
+
+HAVING COUNT(*) > 1 and OwnerName is not null
+) 
+
+select *
+from duplicates
+
+
+-- Delete unused  columns
+
+ALTER TABLE Covid_Potfolio_project..NashvilleHousing
+Drop COLUMN TaxDistrict
+
+select*
+from Covid_Potfolio_project..NashvilleHousing
